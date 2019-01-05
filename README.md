a0601ce4c21f78c1f781c866f19adfda4ae2aebe
Moritz Heusinger <moritz.heusinger@gmail.com>
2019-01-05 19:40:18 +0100

    added compact mode compatibility

--- README.md
+++ README.md
@@ -238,6 +238,9 @@ Here you can find a description of the states and how to use them. The most stat
 
 ## Changelog
 
+### 1.1.0
+* (foxriver76) add compact mode compatibility
+
 ### 1.0.2
 * (foxriver76) use adapter-core module
 
--- main.js
+++ main.js
@@ -7,71 +7,77 @@
 'use strict';
 
 const utils = require('@iobroker/adapter-core'); // Get common adapter utils
-const adapter = new utils.Adapter('sonnen');
+let adapter;
 const request = require('request');
 let polling;
 
-// when adapter shuts down
-adapter.on('unload', callback => {
-    try {
-        clearInterval(polling);
-        adapter.log.info('[END] Stopping sonnen adapter...');
-        adapter.setState('info.connection', false, true);
-        callback();
-    } catch (e) {
-        callback();
-    }
-});
+function startAdapter(options) {
+    options = options || {};
+    Object.assign(options, {
+        name: 'sonnen'
+    });
 
+    adapter = new utils.Adapter(options);
 
-// Some message was sent to adapter instance over message box. Used by email, pushover, text2speech, ...
-adapter.on('message', obj => {
-    if (typeof obj === 'object' && obj.message) {
-        if (obj.command === 'send') {
-            // e.g. send email or pushover or whatever
-            console.log('send command');
-
-            // Send response in callback if required
-            if (obj.callback) adapter.sendTo(obj.from, obj.command, 'Message received', obj.callback);
+    adapter.on('unload', callback => {
+        try {
+            clearInterval(polling);
+            adapter.log.info('[END] Stopping sonnen adapter...');
+            adapter.setState('info.connection', false, true);
+            callback();
+        } catch (e) {
+            callback();
         }
-    }
-});
+    });
 
-// is called when databases are connected and adapter received configuration.
-adapter.on('ready', () => {
-    if (adapter.config.ip) {
-        adapter.log.info('[START] Starting sonnen adapter');
-        main();
-    } else adapter.log.warn('[START] No IP-address set');
-});
+    adapter.on('message', obj => {
+        if (typeof obj === 'object' && obj.message) {
+            if (obj.command === 'send') {
+                // e.g. send email or pushover or whatever
+                console.log('send command');
 
-// is called if a subscribed state changes
-adapter.on('stateChange', (id, state) => {
-    if (!id || !state || state.ack) return; // Ignore acknowledged state changes or error states
-    id = id.substring(adapter.namespace.length + 1); // remove instance name and id
-    state = state.val;
+                // Send response in callback if required
+                if (obj.callback) adapter.sendTo(obj.from, obj.command, 'Message received', obj.callback);
+            }
+        }
+    });
 
-    adapter.log.debug('[COMMAND] State Change - ID: ' + id + '; State: ' + state);
+    adapter.on('ready', () => {
+        if (adapter.config.ip) {
+            adapter.log.info('[START] Starting sonnen adapter');
+            main();
+        } else adapter.log.warn('[START] No IP-address set');
+    });
 
-    if (id === 'control.charge') {
-        request.put('http://' + ip + ':8080/api/v1/setpoint/charge/' + state, (error, response, body) => {
-            if (response && response.statusCode.toString() === '200') {
-                adapter.setState('control.charge', state, true);
-                adapter.log.debug('[PUT] ==> Sent ' + state + ' to charge');
-            } else adapter.log.warn('[PUT] Error ' + error);
-        });
-    } else if (id === 'control.discharge') {
-        request.put('http://' + ip + ':8080/api/v1/setpoint/discharge/' + state, (error, response, body) => {
-            if (response && response.statusCode.toString() === '200') {
-                adapter.setState('control.discharge', state, true);
-                adapter.log.debug('[PUT] ==> Sent ' + state + ' to discharge');
-            } else adapter.log.warn('[PUT] Error ' + error);
-        });
-    } // endElseIf
-});
+    adapter.on('stateChange', (id, state) => {
+        if (!id || !state || state.ack) return; // Ignore acknowledged state changes or error states
+        id = id.substring(adapter.namespace.length + 1); // remove instance name and id
+        state = state.val;
+
+        adapter.log.debug('[COMMAND] State Change - ID: ' + id + '; State: ' + state);
+
+        if (id === 'control.charge') {
+            request.put('http://' + ip + ':8080/api/v1/setpoint/charge/' + state, (error, response, body) => {
+                if (response && response.statusCode.toString() === '200') {
+                    adapter.setState('control.charge', state, true);
+                    adapter.log.debug('[PUT] ==> Sent ' + state + ' to charge');
+                } else adapter.log.warn('[PUT] Error ' + error);
+            });
+        } else if (id === 'control.discharge') {
+            request.put('http://' + ip + ':8080/api/v1/setpoint/discharge/' + state, (error, response, body) => {
+                if (response && response.statusCode.toString() === '200') {
+                    adapter.setState('control.discharge', state, true);
+                    adapter.log.debug('[PUT] ==> Sent ' + state + ' to discharge');
+                } else adapter.log.warn('[PUT] Error ' + error);
+            });
+        } // endElseIf
+    });
+
+    return adapter;
+} // endStartAdapter
+
 
 function main() {
-    // Vars
     const ip = adapter.config.ip;
     const pollingTime = adapter.config.pollInterval || 7000;
     adapter.log.debug('[INFO] Configured polling interval: ' + pollingTime);
@@ -136,7 +142,7 @@ function main() {
  * Internals
  */
 function setBatteryStates(json, cb) {
-    if(json.ReturnCode) {
+    if (json.ReturnCode) {
         adapter.log.warn('[DATA] <== Return Code ' + json.ReturnCode);
         return;
     } // endIf
@@ -165,5 +171,13 @@ function setBatteryStates(json, cb) {
     adapter.setState('status.flowProductionBattery', json.FlowProductionBattery, true);
     adapter.setState('status.flowProductionGrid', json.FlowProductionGrid, true);
 
-    if (cb && typeof(cb) === 'function') return cb();
+    if (cb && typeof (cb) === 'function') return cb();
 } // endSetBatteryStates
+
+// If started as allInOne/compact mode => return function to create instance
+if (module && module.parent) {
+    module.exports = startAdapter;
+} else {
+    // or start the instance directly
+    startAdapter();
+} // endElse
