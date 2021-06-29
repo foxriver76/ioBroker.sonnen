38cf567bc75077991a62c0f39d39e5087081b68a
Moritz Heusinger <moritz.heusinger@gmail.com>
2021-06-29 14:05:19 +0200

    widget template

---
+++ widgets/sonnen/js/sonnen.js
@@ -0,0 +1,73 @@
+/*
+    ioBroker.vis template Widget-Set
+
+    version: "0.0.1"
+
+    Copyright 2021 Author author@mail.com
+*/
+'use strict';
+
+// add translations for edit mode
+$.extend(
+    true,
+    systemDictionary,
+    {
+        // Add your translations here, e.g.:
+        // "size": {
+        // 	"en": "Size",
+        // 	"de": "Größe",
+        // 	"ru": "Размер",
+        // 	"pt": "Tamanho",
+        // 	"nl": "Grootte",
+        // 	"fr": "Taille",
+        // 	"it": "Dimensione",
+        // 	"es": "Talla",
+        // 	"pl": "Rozmiar",
+        // 	"zh-cn": "尺寸"
+        // }
+    }
+);
+
+// this code can be placed directly in sonnen.html
+vis.binds['sonnen'] = {
+    version: '0.0.1',
+    showVersion: function () {
+        if (vis.binds['sonnen'].version) {
+            console.log('Version sonnen: ' + vis.binds['sonnen'].version);
+            vis.binds['sonnen'].version = null;
+        }
+    },
+    createWidget: function (widgetID, view, data, style) {
+        console.log(new Date().toLocaleTimeString() + ' sonnen[' + widgetID + ']: Trying to render widget');
+        const $div = $('#' + widgetID);
+        // if nothing found => wait
+        if (!$div.length) {
+            return setTimeout(function () {
+                vis.binds['sonnen'].createWidget(widgetID, view, data, style);
+            }, 100);
+        }
+
+        let text = '';
+        text += '<img src="widgets/sonnen/img/sonnen.png" alt="sonnen" class="grid"/>';
+        text += '<img src="widgets/sonnen/img/sonnen.png" alt="sonnen" class="house"/>';
+        text += '<img src="widgets/sonnen/img/sonnen.png" alt="sonnen" class="battery"/>';
+        text += '<img src="widgets/sonnen/img/sonnen.png" alt="sonnen" class="photovoltaics"/>';
+
+        $('#' + widgetID).html(text);
+
+        // subscribe on updates of value
+        function onChange(e, newVal, oldVal) {
+            $div.find('.template-value').html(newVal);
+        }
+
+        if (data.oid) {
+            vis.states.bind(data.oid + '.val', onChange);
+            //remember bound state that vis can release if didnt needed
+            $div.data('bound', [data.oid + '.val']);
+            //remember onchange handler to release bound states
+            $div.data('bindHandler', onChange);
+        }
+    }
+};
+
+vis.binds['sonnen'].showVersion();
