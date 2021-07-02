a28979f5b2c8c4b7c70a3ab3e4c00385334fda3f
Moritz Heusinger <moritz.heusinger@gmail.com>
2021-07-02 11:02:38 +0200

    chore: release v1.8.5
    widget: stroke width can now be configured

--- README.md
+++ README.md
@@ -294,6 +294,9 @@ The channel consists of read-only states of type `number`, providing information
 	### __WORK IN PROGRESS__
 -->
 
+### 1.8.5 (2021-07-02)
+* (foxriver76) widget: stroke width can now be configured
+
 ### 1.8.4 (2021-07-01)
 * (foxriver76) widget: we made ID names more adapter specific to avoid getting wrong translations
 
--- widgets/sonnen/js/sonnen.js
+++ widgets/sonnen/js/sonnen.js
@@ -36,6 +36,18 @@ $.extend(
             'es': 'instancia de adaptador',
             'pl': 'instancja adaptera',
             'zh-cn': '适配器实例'
+        },
+        'sonnenStrokeWidth': {
+            'en': 'stroke width in px',
+            'de': 'Strichstärke in px',
+            'ru': 'ширина штриха в пикселях',
+            'pt': 'largura do traço em px',
+            'nl': 'lijnbreedte in px',
+            'fr': 'largeur de trait en px',
+            'it': 'larghezza del tratto in px',
+            'es': 'ancho del trazo en px',
+            'pl': 'szerokość obrysu w pikselach',
+            'zh-cn': '以 px 为单位的描边宽度'
         }
     }
 );
@@ -52,6 +64,9 @@ vis.binds['sonnen'] = {
     createWidget: function (widgetID, view, data, style) {
         let mainColor = data._data.sonnenMainColor || '#181A27';
         let instance = data._data.sonnenInstance || 'sonnen.0';
+        let strokeWidth = !isNaN(parseInt(data._data.sonnenStrokeWidth)) ? data._data.sonnenStrokeWidth + 'px' : '8px';
+        let halfStrokeWidth = parseInt(strokeWidth) / 2 + 'px';
+
         console.log(new Date().toLocaleTimeString() + ' sonnen[' + widgetID + ']: Trying to render widget');
         let $div = $('#' + widgetID);
         // if nothing found => wait
@@ -62,27 +77,27 @@ vis.binds['sonnen'] = {
         }
 
         let text = '';
-        text += '<div class="sonnen-grid"><svg class="sonnen-img" xmlns="http://www.w3.org/2000/svg" width="31" height="33" viewBox="0 0 31 33">\n' +
+        text += '<div id="sonnen-grid"><svg class="sonnen-img" xmlns="http://www.w3.org/2000/svg" width="31" height="33" viewBox="0 0 31 33">\n' +
             '    <g fill="none" fill-rule="evenodd" stroke="' + mainColor + '" stroke-linecap="round" stroke-linejoin="round">\n' +
             '        <path d="M.5 32.5h30M25 32.5L18.523.5h-5.858L6 32.5M19.379 3.5H26.5M4.5 3.5H12M21 11.5h4.5M5.508 11.5H10"/>\n' +
             '        <path d="M19.5 6.5l-9.5 9 14 13"/>\n' +
             '        <path d="M11.5 6.5l9.5 9-14 13"/>\n' +
             '    </g>\n' +
             '</svg>\n</div>';
-        text += '<div class="sonnen-house"><svg class="sonnen-img" xmlns="http://www.w3.org/2000/svg" width="37" height="31" viewBox="0 0 37 31">\n' +
+        text += '<div id="sonnen-house"><svg class="sonnen-img" xmlns="http://www.w3.org/2000/svg" width="37" height="31" viewBox="0 0 37 31">\n' +
             '    <g fill="none" fill-rule="evenodd" stroke="' + mainColor + '" stroke-linecap="round" stroke-linejoin="round">\n' +
             '        <path d="M36.204 12.656L18.602.463 1 12.656"/>\n' +
             '        <path d="M6.5 9v21.5h4v-12h8v12h12V9"/>\n' +
             '    </g>\n' +
             '</svg>\n</div>';
-        text += '<div class="sonnen-battery"><svg class="sonnen-img" xmlns="http://www.w3.org/2000/svg" width="29" height="31" viewBox="0 0 29 31">\n' +
+        text += '<div id="sonnen-battery"><svg class="sonnen-img" xmlns="http://www.w3.org/2000/svg" width="29" height="31" viewBox="0 0 29 31">\n' +
             '    <g fill="none" fill-rule="evenodd" stroke="' + mainColor + '">\n' +
             '        <rect width="28" height="30" x=".5" y=".5" rx="1"/>\n' +
             '        <path stroke-width=".9" d="M14.5 13.5c1.678 0 3.039-1.343 3.039-3s-1.36-3-3.039-3c-1.678 0-3.039 1.343-3.039 3s1.36 3 3.039 3z"/>\n' +
             '        <path stroke-linecap="round" d="M10.5 16.5h8"/>\n' +
             '    </g>\n' +
             '</svg>\n</div>';
-        text += '<div class="sonnen-photovoltaics"><svg class="sonnen-img" xmlns="http://www.w3.org/2000/svg" width="43" height="28" viewBox="0 0 43 28">\n' +
+        text += '<div id="sonnen-photovoltaics"><svg class="sonnen-img" xmlns="http://www.w3.org/2000/svg" width="43" height="28" viewBox="0 0 43 28">\n' +
             '    <g fill="none" fill-rule="evenodd" stroke="' + mainColor + '" stroke-linejoin="round">\n' +
             '        <path d="M36.5.5h-30l-6 22h42z"/>\n' +
             '        <path stroke-width=".5" d="M21.5.5v22M12.5.5l-3 22M30.5.5l3 22"/>\n' +
@@ -90,23 +105,23 @@ vis.binds['sonnen'] = {
             '        <path stroke-linecap="round" d="M31.5 22.602V27.5M11.5 22.5v5"/>\n' +
             '    </g>\n' +
             '</svg>\n</div>';
-        text += '<div class="sonnen-photovoltaics-line"> <svg height="100%" width="100%">\n' +
-            '  <line class="sonnen-photovoltaics-line-svg sonnen-line-svg" x1="0" y1="0" x2="0" y2="5000" />\n' +
+        text += '<div id="sonnen-photovoltaics-line"> <svg height="100%" width="5000">\n' +
+            '  <line id="sonnen-photovoltaics-line-svg" class="sonnen-line-svg" x1="0" y1="0" x2="0" y2="5000" />\n' +
             '</svg> </div>';
-        text += '<div class="sonnen-house-line"> <svg height="100%" width="100%">\n' +
-            '  <line class="sonnen-house-line-svg sonnen-line-svg" x1="0" y1="0" x2="0" y2="5000" />\n' +
+        text += '<div id="sonnen-house-line"> <svg height="100%" width="5000">\n' +
+            '  <line id="sonnen-house-line-svg" class="sonnen-line-svg" x1="0" y1="0" x2="0" y2="5000" />\n' +
             '</svg> </div>';
-        text += '<div class="sonnen-battery-line"> <svg height="210" width="100%">\n' +
-            '  <line class="sonnen-battery-line-svg sonnen-line-svg" x1="0" y1="0" x2="5000" y2="0" />\n' +
+        text += '<div id="sonnen-battery-line"> <svg height="5000" width="100%">\n' +
+            '  <line id="sonnen-battery-line-svg" class="sonnen-line-svg" x1="0" y1="0" x2="5000" y2="0" />\n' +
             '</svg> </div>';
-        text += '<div class="sonnen-grid-line"> <svg height="210" width="100%">\n' +
-            '  <line class="sonnen-grid-line-svg sonnen-line-svg" x1="0" y1="0" x2="5000" y2="0" />\n' +
+        text += '<div id="sonnen-grid-line"> <svg height="5000" width="100%">\n' +
+            '  <line id="sonnen-grid-line-svg" class="sonnen-line-svg" x1="0" y1="0" x2="5000" y2="0" />\n' +
             '</svg> </div>';
-        text += '<div id="sonnen-soc"><p class="sonnen-value" id="sonnen-soc-value"></p></div>';
-        text += '<div id="sonnen-consumption"><p class="sonnen-value" id="sonnen-consumption-value"></p></div>';
-        text += '<div id="sonnen-production"><p class="sonnen-value" id="sonnen-production-value"></p></div>';
-        text += '<div id="sonnen-grid"><p class="sonnen-value" id="sonnen-grid-value"></p></div>';
-        text += '<div id="sonnen-pac"><p class="sonnen-value" id="sonnen-pac-value"></p></div>';
+        text += '<div class="sonnen-soc"><p class="sonnen-value" id="sonnen-soc-value"></p></div>';
+        text += '<div class="sonnen-consumption"><p class="sonnen-value" id="sonnen-consumption-value"></p></div>';
+        text += '<div class="sonnen-production"><p class="sonnen-value" id="sonnen-production-value"></p></div>';
+        text += '<div class="sonnen-grid"><p class="sonnen-value" id="sonnen-grid-value"></p></div>';
+        text += '<div class="sonnen-pac"><p class="sonnen-value" id="sonnen-pac-value"></p></div>';
 
         $('#' + widgetID).html(text);
 
@@ -129,9 +144,9 @@ vis.binds['sonnen'] = {
                     $('#sonnen-grid-value').text(newVal + ' W');
                     if (parseInt(newVal) > 0) {
                         // positive means we are feeding in
-                        $('.sonnen-grid-line-svg').css('animation', 'sonnen-dash 10s linear reverse infinite');
+                        $('#sonnen-grid-line-svg').css('animation', 'sonnen-dash 10s linear reverse infinite');
                     } else {
-                        $('.sonnen-grid-line-svg').css('animation', 'sonnen-dash 10s linear infinite');
+                        $('#sonnen-grid-line-svg').css('animation', 'sonnen-dash 10s linear infinite');
                     }
                     break;
                 case 'pacTotal':
@@ -139,9 +154,9 @@ vis.binds['sonnen'] = {
                     break;
                 case 'batteryCharging':
                     if (newVal) {
-                        $('.sonnen-battery-line-svg').css('animation', 'sonnen-dash 10s linear infinite');
+                        $('#sonnen-battery-line-svg').css('animation', 'sonnen-dash 10s linear infinite');
                     } else {
-                        $('.sonnen-battery-line-svg').css('animation', 'sonnen-dash 10s reverse linear infinite');
+                        $('#sonnen-battery-line-svg').css('animation', 'sonnen-dash 10s reverse linear infinite');
                     }
                     break;
                 case 'flowConsumptionBattery':
@@ -154,33 +169,33 @@ vis.binds['sonnen'] = {
                     if (!(vis.states[instance + '.status.flowConsumptionBattery.val'] || vis.states[instance + '.status.flowGridBattery.val'] ||
                         vis.states [instance + '.status.flowProductionBattery.val'])) {
                         // no flow to battery disable line
-                        $('.sonnen-battery-line').hide();
+                        $('#sonnen-battery-line').hide();
                     } else {
-                        $('.sonnen-battery-line').show();
+                        $('#sonnen-battery-line').show();
                     }
 
                     if (!(vis.states[instance + '.status.flowConsumptionBattery.val'] || vis.states[instance + '.status.flowConsumptionGrid.val'] ||
                         vis.states[instance + '.status.flowConsumptionProduction.val'])) {
                         // no consumption disable house line
-                        $('.sonnen-house-line').hide();
+                        $('#sonnen-house-line').hide();
                     } else {
-                        $('.sonnen-house-line').show();
+                        $('#sonnen-house-line').show();
                     }
 
                     if (!(vis.states[instance + '.status.flowProductionBattery.val'] || vis.states[instance + '.status.flowProductionGrid.val'] ||
                         vis.states[instance + '.status.flowConsumptionProduction.val'])) {
                         // no production disable photovoltaics-line line
-                        $('.sonnen-photovoltaics-line').hide();
+                        $('#sonnen-photovoltaics-line').hide();
                     } else {
-                        $('.sonnen-photovoltaics-line').show();
+                        $('#sonnen-photovoltaics-line').show();
                     }
 
                     if (!(vis.states[instance + '.status.flowGridBattery.val'] || vis.states[instance + '.status.flowProductionGrid.val'] ||
                         vis.states[instance + '.status.flowConsumptionGrid.val'])) {
                         // no grid disable photovoltaics-line line
-                        $('.sonnen-grid-line').hide();
+                        $('#sonnen-grid-line').hide();
                     } else {
-                        $('.sonnen-grid-line').show();
+                        $('#sonnen-grid-line').show();
                     }
                     break;
                 default:
@@ -221,6 +236,19 @@ vis.binds['sonnen'] = {
             $div.data('bound', dps);
             $div.data('bindHandler', onChange);
 
+            // set configured stroke width
+            $('#sonnen-photovoltaics-line').css('width', strokeWidth);
+            $('#sonnen-photovoltaics-line').css('left', 'calc(50% - '+ halfStrokeWidth + ')');
+
+            $('#sonnen-house-line').css('width', strokeWidth);
+            $('#sonnen-house-line').css('left', 'calc(50% - '+ halfStrokeWidth + ')');
+
+            $('#sonnen-battery-line').css('height', strokeWidth);
+            $('#sonnen-battery-line').css('bottom', 'calc(50% - '+ halfStrokeWidth + ')');
+
+            $('#sonnen-grid-line').css('height', strokeWidth);
+            $('#sonnen-grid-line').css('bottom', 'calc(50% - '+ halfStrokeWidth + ')');
+
             // set initial values
             $('#sonnen-soc-value').text(states[instance + '.status.userSoc'].val + ' %');
             $('#sonnen-consumption-value').text(states[instance + '.status.consumption'].val + ' W');
@@ -233,39 +261,39 @@ vis.binds['sonnen'] = {
             if (!(states[instance + '.status.flowConsumptionBattery'].val || states[instance + '.status.flowGridBattery'].val ||
             states [instance + '.status.flowProductionBattery'].val)) {
                 // no flow to battery disable line
-                $('.sonnen-battery-line').hide();
+                $('#sonnen-battery-line').hide();
             }
 
             if (!(states[instance + '.status.flowConsumptionBattery'].val || states[instance + '.status.flowConsumptionGrid'].val ||
             states[instance + '.status.flowConsumptionProduction'].val)) {
                 // no consumption disable house line
-                $('.sonnen-house-line').hide();
+                $('#sonnen-house-line').hide();
             }
 
             if (!(states[instance + '.status.flowProductionBattery'].val || states[instance + '.status.flowProductionGrid'].val ||
                 states[instance + '.status.flowConsumptionProduction'].val)) {
                 // no production disable photovoltaics-line line
-                $('.sonnen-photovoltaics-line').hide();
+                $('#sonnen-photovoltaics-line').hide();
             }
 
             if (!(states[instance + '.status.flowGridBattery'].val || states[instance + '.status.flowProductionGrid'].val ||
                 states[instance + '.status.flowConsumptionGrid'].val)) {
                 // no grid disable photovoltaics-line line
-                $('.sonnen-grid-line').hide();
+                $('#sonnen-grid-line').hide();
             }
 
             // now add the animations
             if (states[instance + '.status.batteryCharging'].val) {
-                $('.sonnen-battery-line-svg').css('animation', 'sonnen-dash 10s linear infinite');
+                $('#sonnen-battery-line-svg').css('animation', 'sonnen-dash 10s linear infinite');
             } else {
-                $('.sonnen-battery-line-svg').css('animation', 'sonnen-dash 10s reverse linear infinite');
+                $('#sonnen-battery-line-svg').css('animation', 'sonnen-dash 10s reverse linear infinite');
             }
 
             if (parseInt(states[instance + '.status.gridFeedIn'].val) > 0) {
                 // positive means we are feeding in
-                $('.sonnen-grid-line-svg').css('animation', 'sonnen-dash 10s linear reverse infinite');
+                $('#sonnen-grid-line-svg').css('animation', 'sonnen-dash 10s linear reverse infinite');
             } else {
-                $('.sonnen-grid-line-svg').css('animation', 'sonnen-dash 10s linear infinite');
+                $('#sonnen-grid-line-svg').css('animation', 'sonnen-dash 10s linear infinite');
             }
         });
     }
